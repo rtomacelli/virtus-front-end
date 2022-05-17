58885d29e21bf5c9c42c519d037ea74a462ef745
rtomacelli <ricardotomacelli@gmail.com>
2022-05-16 22:59:25 -0300

    autocomplete

--- src/app/administration/office/add-office/add-office.component.ts
+++ src/app/administration/office/add-office/add-office.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MatDialog, MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogRef,
+  MatDialogConfig,
+} from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { OfficeService } from '../../../service/office.service';
 
@@ -18,55 +22,76 @@ export interface Chefes {
   templateUrl: './add-office.component.html',
   styleUrls: ['./add-office.component.css'],
 })
-
 export class AddOfficeComponent implements OnInit {
-
-  chefes: any [];
+  chefes: any[];
   Users: any = [];
+  Users2: any = [];
+
   displayedColumns: string[] = ['id', 'name'];
-  options: Chefes[] = [{id: 0, name: 'Mary'}, {id: 1, name: 'Maria'}, {id: 2, name: 'Mario'}, {id: 3, name: 'Marcelo'}, {id: 4, name: 'Shelley'}, {id: 5, name: 'Shannon'}, {id: 6, name: 'Igor'}];
-  
+  options: Chefes[] = [
+    { id: 0, name: 'Mary' },
+    { id: 1, name: 'Maria' },
+    { id: 2, name: 'Mario' },
+    { id: 3, name: 'Marcelo' },
+    { id: 4, name: 'Shelley' },
+    { id: 5, name: 'Shannon' },
+    { id: 6, name: 'Igor' },
+  ];
+
   myControl = new FormControl();
   filteredOptions: Observable<Chefes[]>;
-  @Input() officeObj = { nome: '', abreviatura: '', descricao: '', chefe_id: 0 };
+  @Input() officeObj = {
+    nome: '',
+    abreviatura: '',
+    descricao: '',
+    chefe_id: 0,
+  };
 
   constructor(
     public router: Router,
     public dialog: MatDialog,
     public userService: UserService,
-    public officeService: OfficeService,
+    public officeService: OfficeService
   ) {}
 
   ngOnInit() {
-
-    this.Users = this.userService.getUsers();
-//  this.options = this.Users.stringObject;
-//    delete this.Users.username;
-//    delete this.Users.password;
-//    delete this.Users.email;
-//    delete this.Users.mobile;
-//    delete this.Users.role_id;
-
+    this.Users2 = this.userService.getUsers();
 
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
-      map(value => (typeof value === 'string' ? value : value.name)),
-      map(name => (name ? this._filter(name) : this.options.slice())),
+      map((value) => (typeof value === 'string' ? value : value.name)),
+      map((name) => (name ? this._filter(name) : this.options.slice()))
     );
   }
 
   displayFn(chefes: Chefes): string {
-    if(chefes){
-    let nomeUsuario = chefes ? chefes.name : '';
-    console.log(nomeUsuario);
-    };
+    if (chefes) {
+      let nomeUsuario = chefes ? chefes.name : '';
+      console.log(nomeUsuario);
+      //console.log(idUsuario);
+      //this.officeObj.chefe_id = Number(idUsuario);
+    }
     return chefes && chefes.name ? chefes.name : '';
   }
 
   private _filter(name: string): Chefes[] {
     const filterValue = name.toLowerCase();
-    this.officeObj.chefe_id = this.options.find(option => option.name.toLowerCase().includes(filterValue)).id;
-    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+    if (
+      this.options.find((option) =>
+        option.name.toLowerCase().includes(filterValue)
+      ) == undefined
+    ) {
+      this.officeObj.chefe_id = 0;
+    } else {
+      this.officeObj.chefe_id = this.options.find((option) =>
+        option.name.toLowerCase().includes(filterValue)
+      ).id;
+    }
+    //this.officeObj.chefe_id = this.options.find((option) => option.name.toLowerCase().includes(filterValue)).id;
+    //console.log(this.options.filter((option) =>option.name.toLowerCase().includes(filterValue)));
+    return this.options.filter((option) =>
+      option.name.toLowerCase().includes(filterValue)
+    );
   }
 
   addOffice(data: any) {
